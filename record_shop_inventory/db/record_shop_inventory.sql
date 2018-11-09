845bcfedaa947dd0ec610e91b7a75b7c370c5a28
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-09 12:07:42 +0000

    Tables created

---
+++ record_shop_inventory/db/record_shop_inventory.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS records;
+DROP TABLE IF EXISTS labels;
+
+
+CREATE TABLE labels (
+  id SERIAL8 PRIMARY KEY,
+  name TEXT,
+  location TEXT
+);
+
+CREATE TABLE records (
+  id SERIAL8 PRIMARY KEY,
+  title TEXT,
+  artist TEXT,
+  genre TEXT,
+  description TEXT,
+  stock_quantity INT8,
+  buying_cost INT8,
+  selling_price INT8,
+  label_id INT8 REFERENCES labels(id) ON DELETE CASCADE
+);
+
+CREATE TABLE sales (
+  id SERIAL8 PRIMARY KEY,
+  record_id INT8 REFERENCES records(id) ON DELETE CASCADE,
+  sale_quantity INT8
+);
