07d0a967b127fd8f6c77b53f793116d89fbcb15d
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-09 13:18:37 +0000

    record class def init fixed

--- record_shop_inventory/models/record.rb
+++ record_shop_inventory/models/record.rb
@@ -1,15 +1,17 @@
 class Record
 
   def initialize(options)
-    @id = options[:id].to_i if options[:id]
-    @title = options[:title]
-    @artist = options[:artist]
-    @genre = options[:genre]
-    @description = options[:description]
-    @stock_quantity = options[:stock_quantity].to_i
-    @buying_cost = options[:buying_cost].to_i
-    @selling_price = options[:selling_price].to_i
-    @label_id = options[:label_id].to_i
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @artist = options['artist']
+    @genre = options['genre']
+    @description = options['description']
+    @stock_quantity = options['stock_quantity'].to_i
+    @buying_cost = options['buying_cost'].to_i
+    @selling_price = options['selling_price'].to_i
+    @label_id = options['label_id'].to_i
   end
+
+
 
 end
