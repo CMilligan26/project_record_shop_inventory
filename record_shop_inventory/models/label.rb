3ae0afd600c01c099ca966edbc56a36d5be7edc5
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-09 13:32:45 +0000

    basic label complete

--- record_shop_inventory/models/label.rb
+++ record_shop_inventory/models/label.rb
@@ -2,6 +2,8 @@ require_relative( '../db/sql_runner' )
 
 class Label
 
+  attr_reader :name, :location
+
   def initialize(options)
     @id = options['id'] if options['id']
     @name = options['name']
--- record_shop_inventory/specs/label_spec.rb
+++ record_shop_inventory/specs/label_spec.rb
@@ -6,7 +6,19 @@ require_relative('../models/label')
 class LabelTest < MiniTest::Test
 
 def setup
-  label = Label.new({'name' => 'Test_Label', 'Location' => 'Earth'})
+  @label = Label.new({'name' => 'Test_Label', 'location' => 'Earth'})
+end
+
+def test_label_exists
+  assert_equal(Label, @label.class)
+end
+
+def test_label_has_name
+  assert_equal('Test_Label', @label.name)
+end
+
+def test_label_has_location
+  assert_equal('Earth', @label.location)
 end
 
 end
