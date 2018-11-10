e46c61628f7fe727061773a2e9bd4b6677c81e22
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-10 22:05:03 +0000

    add sale complete

--- record_shop_inventory/controllers/sales.rb
+++ record_shop_inventory/controllers/sales.rb
@@ -7,3 +7,14 @@ get '/sales/all' do
   @sales = Sale.all
   erb ( :"/sales/all" )
 end
+
+get '/sales/new' do
+  @records = Record.all
+  erb ( :"/sales/new" )
+end
+
+post '/sales/new' do
+  sale = Sale.new(params)
+  sale.save
+  redirect to ("/sales/all")
+end
