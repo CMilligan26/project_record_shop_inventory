1a37fa1e5291715b11f5368448537cdb3f75526e
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-11 14:16:25 +0000

    images added and working

--- record_shop_inventory/db/seeds.rb
+++ record_shop_inventory/db/seeds.rb
@@ -27,15 +27,15 @@ label5.save
 
 #RECORDS
 
-record1 = Record.new({'title' => 'Music Has the Right to Children', 'artist' => 'Boards of Canada', 'genre' => 'Electronica', 'description' => 'Released 20 April 1998', 'stock_quantity' => '5', 'buying_cost' => '20', 'selling_price' => '30', 'label_id' => label1.id})
+record1 = Record.new({'title' => 'Music Has the Right to Children', 'artist' => 'Boards of Canada', 'genre' => 'Electronica', 'description' => 'Released 20 April 1998', 'stock_quantity' => '5', 'buying_cost' => '20', 'selling_price' => '30', 'label_id' => label1.id, 'file' => 'boc-mhtrtc.jpg'})
 
-record2 = Record.new({'title' => 'Lost Horizons', 'artist' => 'Lemon Jelly', 'genre' => 'Downtempo', 'description' => 'Released 7 October 2002', 'stock_quantity' => '1', 'buying_cost' => '50', 'selling_price' => '85', 'label_id' => label2.id})
+record2 = Record.new({'title' => 'Lost Horizons', 'artist' => 'Lemon Jelly', 'genre' => 'Downtempo', 'description' => 'Released 7 October 2002', 'stock_quantity' => '1', 'buying_cost' => '50', 'selling_price' => '85', 'label_id' => label2.id, 'file' => 'lj-lh.jpg'})
 
-record3 = Record.new({'title' => 'Mezzanine', 'artist' => 'Massive Attack', 'genre' => 'Trip hop', 'description' => 'Released 20 April 1998', 'stock_quantity' => '3', 'buying_cost' => '15', 'selling_price' => '21', 'label_id' => label3.id})
+record3 = Record.new({'title' => 'Mezzanine', 'artist' => 'Massive Attack', 'genre' => 'Trip Hop', 'description' => 'Released 20 April 1998', 'stock_quantity' => '3', 'buying_cost' => '15', 'selling_price' => '21', 'label_id' => label3.id, 'file' => 'ma-m.jpg'})
 
-record4 = Record.new({'title' => 'Black Sands', 'artist' => 'Bonobo', 'genre' => 'Electronic', 'description' => 'Released 29 March 2010', 'stock_quantity' => '2', 'buying_cost' => '20', 'selling_price' => '13', 'label_id' => label4.id})
+record4 = Record.new({'title' => 'Black Sands', 'artist' => 'Bonobo', 'genre' => 'Electronic', 'description' => 'Released 29 March 2010', 'stock_quantity' => '2', 'buying_cost' => '20', 'selling_price' => '13', 'label_id' => label4.id, 'file' => 'b-bs.jpg'})
 
-record5 = Record.new({'title' => 'Since I Left You', 'artist' => 'The Avalanches', 'genre' => 'Plunderphonics', 'description' => 'Released 27 November 2000', 'stock_quantity' => '2', 'buying_cost' => '25', 'selling_price' => '35', 'label_id' => label5.id})
+record5 = Record.new({'title' => 'Since I Left You', 'artist' => 'The Avalanches', 'genre' => 'Plunderphonics', 'description' => 'Released 27 November 2000', 'stock_quantity' => '2', 'buying_cost' => '25', 'selling_price' => '35', 'label_id' => label5.id, 'file' => 'ta-sily.jpg'})
 
 record1.save
 record2.save
