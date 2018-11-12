ab6bb1632945525e8f08a815d5422d0e5407bf98
Christopher Milligan <milliganjchristopher@gmail.com>
2018-11-12 18:37:36 +0000

    added artist, genre and categorizations tables

--- record_shop_inventory/db/record_shop_inventory.sql
+++ record_shop_inventory/db/record_shop_inventory.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS genre_categorizations;
 DROP TABLE IF EXISTS sales;
 DROP TABLE IF EXISTS records;
 DROP TABLE IF EXISTS labels;
-
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS artists;
 
 CREATE TABLE labels (
   id SERIAL8 PRIMARY KEY,
@@ -9,12 +11,21 @@ CREATE TABLE labels (
   location TEXT
 );
 
+CREATE TABLE artists (
+  id SERIAL8 PRIMARY KEY,
+  artist_name TEXT
+);
+
+CREATE TABLE genres (
+  id SERIAL8 PRIMARY KEY,
+  genre_name TEXT
+);
+
 CREATE TABLE records (
   id SERIAL8 PRIMARY KEY,
   title TEXT,
-  artist TEXT,
-  genre TEXT,
-  description TEXT,
+  artist_id INT8 REFERENCES artists(id) ON DELETE CASCADE,
+  release_date TEXT,
   running_stock_total INT8,
   stock_quantity INT8,
   total_sold INT8,
@@ -22,6 +33,12 @@ CREATE TABLE records (
   selling_price INT8,
   label_id INT8 REFERENCES labels(id) ON DELETE CASCADE,
   file TEXT
+);
+
+CREATE TABLE genre_categorizations (
+  id SERIAL8 PRIMARY KEY,
+  record_id INT8 REFERENCES records(id) ON DELETE CASCADE,
+  genre_id INT8 REFERENCES genres(id) ON DELETE CASCADE
 );
 
 CREATE TABLE sales (
